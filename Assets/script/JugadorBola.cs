9b86ca12b4b7a65ee6d3aed51a375fd119e755b4
robelora <roberto.loramontero@alum.uca.es>
2024-02-28 12:59:34 +0100

    fondo y poco mas
    creado prefabs de suelo para nivel 2
    fondo nuevo

--- Assets/script/JugadorBola.cs
+++ Assets/script/JugadorBola.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 public class JugadorBola : MonoBehaviour
-{
+{   
     // Start is called before the first frame update
     public Camera camara;
     public GameObject suelo;
